f14d928a5ac2002158632691cb3b9b0c3cbd00f0
MungarllaSaiCharitha <mungarllasaicharitha@gmail.com>
2024-11-08 12:36:01 +0530

    in

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.get("/status", (req, res) => {
+  res.json({ message: "API is running successfully" });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
